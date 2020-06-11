4eeceac30b745c1083791303442d8e2b3b776b09
brittle <brittle@brittles-iMac.local>
2020-06-11 23:26:51 +0100

    [no message]

--- conf tuebingen 2020-06/www.slides/s3sync.sh
+++ conf tuebingen 2020-06/www.slides/s3sync.sh
@@ -4,4 +4,4 @@ docpad clean
 
 docpad generate --env static
 
-s3cmd sync out/ s3://www.butterfill.com/talk-slides/mindreading_ww/ --add-header "Cache-Control: max-age=86400"
+s3cmd sync out/ s3://www.butterfill.com/talk-slides/tuebingen_2020_action_problem/ --add-header "Cache-Control: max-age=86400"
